6daf7e663f020042088754a585fda8665b4d28d3
bunicholas <nicholascarroll123@gmail.com>
2021-03-26 16:03:47 -0400

    message

---
+++ week1/selecting/index2.js
@@ -0,0 +1,31 @@
+var newListItem = document.createElement('li')
+newListItem.textContent = '3'
+var myList = document.getElementById('my-list')
+myList.appendChild(newListItem)
+
+var newP = document.createElement('p')
+newP.textContent = 'Bryan University is AWEOMSE!'
+newP.style.textAlign = 'center'
+newP.style.fontSize = '30px'
+document.body.appendChild(newP)
+
+var newList = document.createElement('ol')
+newList.textContent = '3'
+document.body.appendChild(newList)
+
+var newListItem1 = document.createElement('li')
+newListItem1.textContent = '3'
+newList.appendChild(newListItem1)
+
+var newListItem2 = document.createElement('li')
+newListItem2.textContent = '3'
+newList.appendChild(newListItem2)
+
+var newListItem3 = document.createElement('li')
+newListItem3.textContent = '3'
+newList.appendChild(newListItem3)
+
+var newheading = document.createElement('h1')
+newheading.textContent = '3'
+newList.appendChild(newheading)
+
