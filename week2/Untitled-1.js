0468e115b49d4d67634a50a60019a15310288e10
bunicholas <nicholascarroll123@gmail.com>
2021-03-27 22:40:14 -0400

    message

---
+++ week2/Untitled-1.js
@@ -0,0 +1,41 @@
+var newP = document.createElement('p')
+newP.textContent = 'Bryan University is AWEOMSE!'
+newP.style.textAlign = 'center'
+newP.style.fontSize = '30px'
+document.body.appendChild(newP)
+
+var newList = document.createElement('ol')
+newList.textContent = '3'
+document.body.appendChild(newList)
+
+var newListItem1 = document.createElement('li')
+newListItem1.textContent = '3'
+newList.appendChild(newListItem1)
+
+var newListItem2 = document.createElement('li')
+newListItem2.textContent = '3'
+newList.appendChild(newListItem2)
+
+var newListItem3 = document.createElement('li')
+newListItem3.textContent = '3'
+newList.appendChild(newListItem3)
+
+var newheading = document.createElement('h1')
+newheading.textContent = '3'
+newList.appendChild(newheading)
+
+var newfooter = document.createElement('footer')
+newfooter.textContent = '3'
+document.body.append(newfooter)
+
+var newnav = document.createElement('nav')
+newnav.textContent = '3'
+document.body.append(newnav)
+
+var newlink = document.createElement('a')
+newlink.textContent = '3'
+newnav.append(newlink)
+
+var newanchor = document.createElement('a')
+newanchor.textContent = '3'
+newnav.append(newanchor)
