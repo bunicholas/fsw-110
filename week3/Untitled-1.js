fabefaca597826c8f224d1f053bd002ef2aa8413
bunicholas <nicholascarroll123@gmail.com>
2021-04-13 16:35:46 -0400

    message

---
+++ week3/Untitled-1.js
@@ -0,0 +1,8 @@
+var newheading = document.createElement('h2')
+    newheading.textContent = 'Hello World'
+    document.body.appendChild(newheading)
+    newheading.style.fontSize = '20px'
+    newheading.style.fontWeight = 'lighter'
+    newheading.style.fontFamily = 'sans-serif'
+    newheading.style.color = 'cornflowerblue'
+    newheading.classList.add("border");
