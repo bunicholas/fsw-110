864abb149f5bcabdc47d11662868e0ea92037883
bunicholas <nicholascarroll123@gmail.com>
2021-04-29 20:43:32 -0400

    message

---
+++ week5/radio.js
@@ -0,0 +1,13 @@
+const form = document.myForm
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log(form.decision)
+ document.getElementById('fname').value ;
+alert(document.getElementById('fname').value + 
+document.getElementById('lname').value + document.getElementById('age').value )
+
+        alert("First name: Nicholas, Last name: Carroll, Age: 20, Gender: Male,");
+
+    
+})
