da220bca812e964f4e6214b2fbacd4a70a5f797c
bunicholas <nicholascarroll123@gmail.com>
2021-05-05 20:09:57 -0400

    message

--- week4/Cal.js
+++ week4/Cal.js
@@ -11,7 +11,7 @@ function subtract(num1 , num2){
     var number3 = number1 - number2
     document.getElementById("subtracted").innerHTML = number3;
     } 
-    function multiply(num1 , num2){
+    function multiple(num1 , num2){
         var number1 = document.getElementById("num1").value
         var number2 = document.getElementById("num2").value
         var number3 = number1 * number2
